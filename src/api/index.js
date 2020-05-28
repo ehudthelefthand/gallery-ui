45123f263a93a7b59e44bf93a75db0f82c052dbc
Peerawat Poombua <peerawat@odds.team>
2020-05-28 10:21:49 +0700

    now we can delete gallery

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,19 @@
-export async function listGallery() {
-  return [
-    { id: 1, name: "gallery - A" },
-    { id: 2, name: "gallery - B" },
-  ];
+import axios from "axios";
+
+// const host = "http://localhost:8080";
+const host = "/api";
+
+export function listGallery() {
+  const url = `${host}/galleries`;
+  return axios.get(url);
+}
+
+export function createGallery({ name }) {
+  const url = `${host}/galleries`;
+  return axios.post(url, { name });
+}
+
+export function deleteGallery(id) {
+  const url = `${host}/galleries/${id}`;
+  return axios.delete(url);
 }
--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,17 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { listGallery } from "../api";
+import { listGallery, createGallery, deleteGallery } from "../api";
 
 export default function AdminPage() {
   const [galleries, setGalleries] = useState([]);
+  const [galleryName, setGalleryName] = useState("");
 
   useEffect(() => {
     listGallery()
-      .then((data) => {
-        setGalleries(data);
+      .then((res) => {
+        setGalleries(res.data);
       })
       .catch((err) => console.error(err));
   }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (galleryName.trim() === "") {
+      return;
+    }
+    createGallery({ name: galleryName })
+      .then((res) => {
+        const { id, name } = res.data;
+        setGalleries([...galleries, { id, name }]);
+      })
+      .catch((err) => console.error(err));
+    setGalleryName("");
+  };
+
+  const handleDelete = (galleryID) => {
+    deleteGallery(galleryID)
+      .then(() => {
+        setGalleries([...galleries.filter((g) => g.id !== galleryID)]);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div>
       <nav class="nav">
@@ -20,10 +44,16 @@ export default function AdminPage() {
         </Link>
       </nav>
       <div class="container">
-        <form class="gallery-form">
+        <form class="gallery-form" onSubmit={handleSubmit}>
           <div class="gallery-form__field">
             <label for="gallery-name">Gallery Name</label>
-            <input type="text" id="gallery-name" autoFocus />
+            <input
+              type="text"
+              id="gallery-name"
+              autoFocus
+              value={galleryName}
+              onChange={(e) => setGalleryName(e.target.value)}
+            />
           </div>
           <button type="submit" class="gallery-form__submit">
             submit
@@ -38,13 +68,18 @@ export default function AdminPage() {
             </tr>
           </thead>
           <tbody class="gallery-list__body">
-            {galleries.map((gallery) => {
+            {galleries.map((gallery, index) => {
               return (
                 <tr key={gallery.id} class="gallery-list__row">
-                  <td>3</td>
+                  <td>{index + 1}</td>
                   <td>{gallery.name}</td>
                   <td>
-                    <button class="gallery-list__action">delete</button>
+                    <button
+                      class="gallery-list__action"
+                      onClick={() => handleDelete(gallery.id)}
+                    >
+                      delete
+                    </button>
                   </td>
                 </tr>
               );
