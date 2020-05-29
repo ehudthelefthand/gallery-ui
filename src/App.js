9e4d6eab86e577ae352e08795ff7c8209a63300b
Peerawat Poombua <peerawat@odds.team>
2020-05-29 23:09:26 +0700

    added redirection

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React, { createContext } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import AdminPage from "./pages/AdminPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 
+function AuthenticatedApp() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/admin">
+          <AdminPage />
+        </Route>
+        <Route exact path="/">
+          <div>home</div>
+        </Route>
+        <Route path="*">
+          <Redirect to={{ pathname: "/admin" }} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function UnauthenticatedApp() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/signup">
+          <SignupPage />
+        </Route>
+        <Route exact path="/">
+          <div>home</div>
+        </Route>
+        <Route path="*">
+          <Redirect to={{ pathname: "/" }} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
 function App() {
+  let user = {};
   return (
     <div className="App">
-      <Router>
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      {/* <Router>
         <Navbar />
         <Switch>
           <Route exact path="/admin">
@@ -25,7 +73,7 @@ function App() {
             <div>home</div>
           </Route>
         </Switch>
-      </Router>
+      </Router> */}
     </div>
   );
 }
